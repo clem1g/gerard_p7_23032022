84286a9737a6432760537532b4fb449c08c61ecb
clem1g <clemsg@hotmail.fr>
2022-05-23 09:26:36 +0200

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require("sequelize");
+
+const db = new Sequelize({
+  dialect: "mysql",
+  host: "127.0.0.1",
+  username: "root",
+  password: "",
+  database: "groupomania",
+});
+db.authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+module.exports = db;
